e11cc7fbe3316026607478527ddc086ab20c5838
Josh Smith <joshuamsmith413@gmail.com>
2022-01-25 17:30:45 -0500

    JS: one loader issue remains

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  "plugins": [
+    ["module-resolver", {
+      "alias": {
+        "^react-native$": "react-native-web"
+      }
+    }]
+  ],
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+  query: {
+      presets: ['react-native', 'es2015']
+  }
 };
